fdbe1ccda473a91cfe2d40eb045e98fdeca088a6
Jake Dempsey <angelo0000@gmail.com>
2008-03-06 17:42:14 -0600

    added compare to option

---
+++ test/has_many_tenses_with_compare_to_test.rb
@@ -0,0 +1,49 @@
+require(File.join(File.dirname(__FILE__), 'test_helper'))
+
+class Comment < ActiveRecord::Base
+  belongs_to :post
+  has_many_tenses :compare_to => :date
+end
+
+class HasManyTensesWithCompareToTest < Test::Unit::TestCase
+  
+  def test_future_instance
+    assert Comment.new({:date => 15.seconds.from_now}).future?
+    assert !Comment.new({:date => 15.seconds.from_now}).past?
+    assert !Comment.new({:date => 15.seconds.from_now}).recent?
+  end
+  
+  def test_past_instance
+    assert Comment.new({:date => 15.seconds.ago}).past?
+    assert !Comment.new({:date => 15.seconds.ago}).future?
+    assert Comment.new({:date => 15.seconds.ago}).recent?
+  end
+  
+  def test_recent_instance
+    assert Comment.new({:date => 15.seconds.ago}).recent?
+    assert !Comment.new({:date => 15.seconds.ago}).future?
+    assert Comment.new({:date => 15.seconds.ago}).past?
+  end
+  
+  def test_future_has_many_association_proxy
+    post = create_posts_and_comments_with_date({:date => 15.seconds.from_now})
+    assert_equal 5, post.comments.future.size
+    assert_equal 0, post.comments.past.size
+    assert_equal 0, post.comments.recent.size
+  end
+
+  def test_past_has_many_association_proxy
+    post = create_posts_and_comments_with_date({:date => 15.seconds.ago})
+    assert_equal 0, post.comments.future.size
+    assert_equal 5, post.comments.past.size
+    assert_equal 5, post.comments.recent.size
+  end
+
+  def test_past_and_no_recent_has_many_association_proxy
+    post = create_posts_and_comments_with_date({:date => 15.days.ago})
+    assert_equal 0, post.comments.future.size
+    assert_equal 5, post.comments.past.size
+    assert_equal 0, post.comments.recent.size
+  end
+  
+end
