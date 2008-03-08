69b047bc548d2ad2b9c3958a6c3e87e49a94c094
Jake Dempsey <angelo0000@gmail.com>
2008-03-07 18:43:33 -0600

    updated readme

--- test/schema.rb
+++ test/schema.rb
@@ -1,30 +1,30 @@
 ActiveRecord::Schema.define(:version => 0) do
-  
+
   create_table :posts, :force => true do |t|
     t.string        :title
     t.text          :body
     t.timestamps
   end
-  
+
   create_table :comments, :force => true do |t|
     t.references        :post
     t.timestamps
     t.text              :body
     t.time              :date
   end
-  
+
   create_table :second_comments, :force => true do |t|
     t.references        :post
     t.timestamps
     t.text              :body
     t.time              :date
   end
-  
+
   create_table :third_comments, :force => true do |t|
     t.references        :post
     t.timestamps
     t.text              :body
     t.time              :date
   end
-  
+
 end
